761b6e345942118b43e20a371c3f0c93c6c49c42
matthew harwood <matthhar12@gmail.com>
2017-02-02 20:02:29 -0800

    Updated AngularCLI for windows
    Angular CLI was breaking.  Updated with
    https://github.com/angular/angular-cli/issues/4368

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,28 +1,10 @@
-import {Component, OnInit} from '@angular/core';
-import {Http} from '@angular/http';
-import 'rxjs/add/operator/map'
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  title = 'app works!';
-  people: any;
-
-  constructor(http: Http){
-    http.get('http://localhost:8000/api/v1/todoist')
-      // Call map on the response observable to get the parsed people object
-      .map(res => res.json())
-      .subscribe((people) => {
-        console.log(people);
-        this.people = people;
-      });
-
-  }
-
-  ngOnInit() {
-    // console.log('noasdf', window['fetch']("http://localhost:8000/api/v1/todoist", {method: 'get'}).then((response) => console.log(response.json())));
-  }
+export class AppComponent {
+  title = 'app works!22211111';
 }
