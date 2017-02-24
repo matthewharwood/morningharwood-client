a09a806169d42ce76a1ee5fb3174c017a668a2f8
Matthew Harwood <matthhar12@gmail.com>
2017-02-23 20:15:09 -0800

    - added updateds to konami code.

--- client/src/app/keyboard/konami.directive.ts
+++ client/src/app/keyboard/konami.directive.ts
@@ -1,48 +1,37 @@
-import { Directive, HostListener, Input, OnInit } from '@angular/core';
-import { KeyCodes } from './key-codes.enum';
-import { Observable, ReplaySubject } from "rxjs";
+import {Directive, HostListener, Input, OnInit} from '@angular/core';
+import {KeyCodes} from './key-codes.enum';
+import {Observable} from "rxjs";
+import 'rxjs/add/operator/every';
 
 @Directive({
   selector: '[mhKonami]'
 })
 export class KonamiDirective implements OnInit {
-  private subject:ReplaySubject<KeyboardEvent>;
-  @Input('mhKonami') konamiCode: Array<string>;
-
-  constructor() {
-    this.subject = new ReplaySubject(10);
-  }
+  @Input('mhKonami') konamiCode: Array<string>|boolean;
+  public setClass:boolean = false;
 
   ngOnInit() {
 
-    this.subject.subscribe({
-      next: (v) => console.log('observerA: ' + v.keyCode)
+    const konami = [
+      KeyCodes.UP_ARROW,
+      KeyCodes.UP_ARROW,
+      KeyCodes.DOWN_ARROW,
+      KeyCodes.DOWN_ARROW,
+      KeyCodes.LEFT_ARROW,
+      KeyCodes.RIGHT_ARROW,
+      KeyCodes.LEFT_ARROW,
+      KeyCodes.RIGHT_ARROW,
+      KeyCodes.KEY_B,
+      KeyCodes.KEY_A,
+      KeyCodes.ENTER,
+    ];
+
+    const keys = Observable.fromEvent(document, 'keyup');
+    const buffered: any = keys.bufferCount(11, 1);
+    buffered.subscribe(x => {
+      x.every((y, i) => y.keyCode === konami[i])
     });
-      
-    let obj = Observable.fromEvent(document, 'keyup');
-    obj.subscribe(this.subject);
+
     console.debug("⬆', '⬆', '⬇', '⬇', '⬅', '➡', '⬅', '➡', 'b', 'a', 'enter', 'https://en.wikipedia.org/wiki/Konami_Code'");
   }
-  //
-  // @HostListener('document:keyup', ['$event']) onKeypress(event: KeyboardEvent) {
-  //
-  //
-  //   const buffer = [
-  //     KeyCodes.UP_ARROW,
-  //     KeyCodes.UP_ARROW,
-  //     KeyCodes.DOWN_ARROW,
-  //     KeyCodes.DOWN_ARROW,
-  //     KeyCodes.LEFT_ARROW,
-  //     KeyCodes.RIGHT_ARROW,
-  //     KeyCodes.LEFT_ARROW,
-  //     KeyCodes.RIGHT_ARROW,
-  //     KeyCodes.KEY_B,
-  //     KeyCodes.KEY_A,
-  //     KeyCodes.ENTER
-  //   ];
-  //   //
-  //   // let observable = Observable.from([event]);
-  //   // // console.log(event, this.konamiCode);
-  //   // observable.subscribe(this.subject);
-  // }
 }
