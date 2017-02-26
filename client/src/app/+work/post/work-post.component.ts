9620859d4032eed5129172b0315b587406b19a14
matthew harwood <matthhar12@gmail.com>
2017-02-26 10:11:15 -0800

    Added work-post component.
    The work post component will be the details component.

---
+++ client/src/app/+work/post/work-post.component.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by matth on 2/24/2017.
+ */
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'work-post',
+  templateUrl: 'work-post.component.html',
+  styleUrls: ['work-post.component.scss'],
+})
+export class WorkPostComponent {
+}
+
