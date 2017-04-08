d979c3f283d0e4287c1b8347da1c9f262c1d498f
matthew harwood <matthhar12@gmail.com>
2017-04-08 10:16:29 -0700

    - Added documentation on sun-dial-child.

--- client/src/app/sun-dial/sun-dial-child.directive.ts
+++ client/src/app/sun-dial/sun-dial-child.directive.ts
@@ -3,6 +3,16 @@ import { getPartOfDay, PartsOfDay } from '../_handies/date';
 import { Store } from '@ngrx/store';
 import { AppState } from '../app.interface';
 
+/**
+ *  @whatItDoes Take a host and dispatch an event to change part of day.
+ *  @example <input sunDialChild>
+ *  @USAGE
+ *
+ *  @ngModule()
+ *  imports: [
+ *   SunDialModule
+ *  ]
+ */
 @Directive({
   selector: '[sunDialChild]'
 })
