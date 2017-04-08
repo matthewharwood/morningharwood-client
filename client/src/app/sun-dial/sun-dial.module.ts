3494ba03c712c2f7d70a4bf3cb6779c1a0f3a617
matthew harwood <matthhar12@gmail.com>
2017-04-08 10:21:04 -0700

    - Added sundial documentations on module.

--- client/src/app/sun-dial/sun-dial.module.ts
+++ client/src/app/sun-dial/sun-dial.module.ts
@@ -8,7 +8,18 @@ import { SunDialDirective } from "./sun-dial.directive";
 import { FormsModule } from "@angular/forms";
 import { SunDialChildDirective } from './sun-dial-child.directive';
 
-
+/**
+ *  @whatItDoes Detects the time of day and offers a set of directive and components to fetch,hook and override the
+ *  selected time of day.
+ *  @imagine That you can use the Time of day to change the UI. If it's daytime and your website is sepia color scheme
+ *  if it was nightt ime than it has a dark theme.
+ *  @example <input sunDialChild>
+ *
+ *  @ngModule()
+ *  imports: [
+ *   SunDialModule
+ *  ]
+ */
 @NgModule({
   imports: [CommonModule, FormsModule],
   declarations: [SunDialComponent, SunDialDirective, SunDialChildDirective],
